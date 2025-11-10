fc05c412a06327c0ede514ee0660228f5ad29a41
Yan4S <lilysangy@gmail.com>
2025-11-09 19:42:24 -0500

    Add clear_output import to DataExplore.py

--- utils/DataExplore.py
+++ utils/DataExplore.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from typing import List, Dict, Optional
-from IPython.display import display, HTML
+from IPython.display import display, HTML, clear_output
 import matplotlib.pyplot as plt
 import seaborn as sns
 import ipywidgets as widgets
